33045e428eb37a81af57b788579b11df19c9ce44
sohaniiiiii <sohanisiddamsetty@gmail.com>
2025-09-21 20:27:15 +0530

    Update App.java

--- src/main/java/SE/CampusMgmtSyatem/App.java
+++ src/main/java/SE/CampusMgmtSyatem/App.java
@@ -9,5 +9,29 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "LAB INTERNAL Hello World!" );
+        // file->import->smart import->clone url->paste the url->next->next->finish
+        // right click->go to pom.xml then solve the errors then save it and 
+        // again right click prj then click on runas
+        // war ->run on server and jar ->clean install test then run as administrator
+        // now open in server
+
+        // click on prj nd then show in local terminal then git bash
+        // git init
+        // git config --global user.name "" nf user.email ""
+        // git add .
+        // git commit -m ""
+        // git remote add origin "url"
+        // git remote set-url origin "url"
+        // git push -u origin
+
+        // docker open cheyy
+        // next run windows as administrator
+        // docker login
+        // cd move to prj folder
+        // notepad Dockerfile
+        // docker build -t name:tag .
+        // docker run -d -p 8080:8080(or 8009) name:tag
+        // docker tag name:tag user_name/name:tag
+        // docker push user_name/name:tag
     }
 }
